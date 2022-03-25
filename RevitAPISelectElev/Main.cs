08fd642488dad8c459941a3d34a9e7c9f50d4c66
Andrey Salnikov <kvanta13@yandex.ru>
2022-03-25 23:12:43 +0300

    123

--- RevitAPISelectElev/Main.cs
+++ RevitAPISelectElev/Main.cs
@@ -14,7 +14,7 @@ namespace RevitAPISelectElev
     [Transaction(TransactionMode.Manual)]
     public class Main : IExternalCommand
     {
-        public object DuctLeveL { get; private set; }
+        private ElementId levelId;
 
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
@@ -22,31 +22,45 @@ namespace RevitAPISelectElev
             UIDocument uidoc = uiapp.ActiveUIDocument;
             Document doc = uidoc.Document;
 
-            var ductsList = new FilteredElementCollector(doc)
-                .OfClass(typeof(Duct))
-                .Cast<Duct>()
-                .ToList();
+            FilteredElementCollector levels = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_Levels)
+                .WhereElementIsNotElementType();
 
-            var duct1level = new List<Duct>();
-            var duct2Level = new List<Duct>();
+            var duct = new FilteredElementCollector(doc)
+                    .OfCategory(BuiltInCategory.OST_DuctCurves)
+                    .WhereElementIsNotElementType()
+                    .WherePasses(new ElementLevelFilter(levelId));
 
-            string Level = string.Empty;
-            foreach (Duct duct in ductsList)
+            foreach (var level in levels)
             {
-                Parameter DuctLevel = duct.get_Parameter(BuiltInParameter.RBS_START_LEVEL_PARAM);
-                string L = DuctLeveL.AsValueString().ToString();
-                if (L=="Level 1")
-                {
-                    duct1level.Add(duct);
-                }
-                if (L == "Level 2")
-                {
-                    duct2Level.Add(duct);
-                }
+                duct = new FilteredElementCollector(doc)
+                    .OfCategory(BuiltInCategory.OST_DuctCurves)
+                    .WhereElementIsNotElementType()
+                    .WherePasses(new ElementLevelFilter(levelId));
             }
-            Level += $"Количество 1 этаж: {duct1level.Count}{Environment.NewLine} Количество 2 этаж: {duct2Level.Count}";
-            Level += $"ObschKol-vo: {ductsList.Count}";
-            TaskDialog.Show("Kol-vo", Level);
+
+
+            //var duct1level = new List<Duct>();
+            //var duct2Level = new List<Duct>();
+
+            //string Level = string.Empty;
+            //foreach (Duct duct in ductsList)
+            //{
+            //    Parameter DuctLevel = duct.get_Parameter(BuiltInParameter.RBS_START_LEVEL_PARAM);
+            //    string L = DuctLeveL.AsValueString().ToString();
+            //    if (L == "Level 1")
+            //    {
+            //        duct1level.Add(duct);
+            //    }
+            //    if (L == "Level 2")
+            //    {
+            //        duct2Level.Add(duct);
+            //    }
+            //}
+            //Level += $"Количество 1 этаж: {duct1level.Count}{Environment.NewLine} Количество 2 этаж: {duct2Level.Count}";
+            //Level += $"ObschKol-vo: {ductsList.Count}";
+            TaskDialog.Show("123", $"{levels.ToString()} {Environment.NewLine} {duct.ToString()}");
+
             return Result.Succeeded;
         }
     }
